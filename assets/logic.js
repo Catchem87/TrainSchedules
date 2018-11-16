ee0b33b9c262c69140d0b737bd1d87536546e89a
Catchem87 <moras.ashley87@gmail.com>
2018-11-15 22:39:27 -0500

    html layout

---
+++ assets/logic.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA6Z2VNP93ZZS1fUX6WQvQ8eQZn4l3q2Gg",
+    authDomain: "trainschedule-dda4f.firebaseapp.com",
+    databaseURL: "https://trainschedule-dda4f.firebaseio.com",
+    projectId: "trainschedule-dda4f",
+    storageBucket: "",
+    messagingSenderId: "847540890855"
+};
+firebase.initializeApp(config);
